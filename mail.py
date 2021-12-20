6dfd45a21df3544e6d1ad118e743c12232528fc2
Maanuj Vora <maanujvora@gmail.com>
2021-12-20 11:40:16 -0800

    Update mail.py

--- mail.py
+++ mail.py
@@ -32,25 +32,30 @@ for store in game_store:
     # Create the body of the message (an HTML version).
     with open(store.html_path(), 'r') as file:
         html = store.add_to_html(file.read())
+        if !html:
+            pass
+        else:
 
-    # Record the MIME types of both parts - text/plain and text/html.
-    body_plain = MIMEText("Check Out Today's Free Games!F", 'plain')
-    body_html = MIMEText(html, 'html')
+            # Record the MIME types of both parts - text/plain and text/html.
+            body_plain = MIMEText("Check Out Today's Free Games!F", 'plain')
+            body_html = MIMEText(html, 'html')
 
-    # Attach parts into message container.
-    msg.attach(body_plain)
-    msg.attach(body_html)
+            # Attach parts into message container.
+            msg.attach(body_plain)
+            msg.attach(body_html)
 
-    # Send the message via local SMTP server.
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    # s.set_debuglevel(1)
-    s.ehlo()
-    s.starttls()
-    s.login(fromaddr, PASSWORD)
+            # Send the message via local SMTP server.
+            s = smtplib.SMTP('smtp.gmail.com', 587)
+            # s.set_debuglevel(1)
+            s.ehlo()
+            s.starttls()
+            s.login(fromaddr, PASSWORD)
 
-    for id in range(0, len(all_emails), 400):
-        def sendmail(bcc):
-            s.sendmail(fromaddr, toaddr + cc + bcc, str(msg))
-        sendmail(list(all_emails)[id: id + 400])
+            for id in range(0, len(all_emails), 400):
+                def sendmail(bcc):
+                    s.sendmail(fromaddr, toaddr + cc + bcc, str(msg))
+                sendmail(list(all_emails)[id: id + 400])
 
-    s.quit()
+            s.quit()
+    file.close()
+       
