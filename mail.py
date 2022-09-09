49058ecc00112bf0ca82dc1b795c09651df28955
Maanuj Vora <maanujvora@gmail.com>
2022-09-08 22:20:43 -0700

    Update mail.py

--- mail.py
+++ mail.py
@@ -4,58 +4,61 @@ from email.mime.text import MIMEText
 from scripts import steam, db
 import os
 
-game_store = [steam]
+def main():
+    game_store = [steam]
 
 
-PASSWORD = os.environ.get("APP_PASS")
-fromaddr = os.environ.get("APP_EMAIL")
+    PASSWORD = os.environ.get("APP_PASS")
+    fromaddr = os.environ.get("APP_EMAIL")
 
-toaddr = [fromaddr]
-cc = []
-bcc = []
+    toaddr = [fromaddr]
+    cc = []
+    bcc = []
 
-subject = 'FGN - Free Game Notifier'
+    subject = 'FGN - Free Game Notifier'
 
-db.db_connect()
+    db.db_connect()
+    print("db connection successful")
 
-for store in game_store:
+    for store in game_store:
 
-    all_emails = store.get_emails()
+        all_emails = store.get_emails()
 
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = subject
-    msg['From'] = fromaddr
-    msg['To'] = ', '.join(toaddr)
-    # msg['Cc'] = ', '.join(cc)
-    # msg['Bcc'] = ', '.join(bcc)
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = subject
+        msg['From'] = fromaddr
+        msg['To'] = ', '.join(toaddr)
+        # msg['Cc'] = ', '.join(cc)
+        # msg['Bcc'] = ', '.join(bcc)
 
-    # Create the body of the message (an HTML version).
-    with open(store.html_path(), 'r') as file:
-        html = store.add_to_html(file.read())
-        if not html:
-            pass
-        else:
+        # Create the body of the message (an HTML version).
+        with open(store.html_path(), 'r') as file:
+            html = store.add_to_html(file.read())
+            if not html:
+                pass
+            else:
 
-            # Record the MIME types of both parts - text/plain and text/html.
-            body_plain = MIMEText("Check Out Today's Free Games!F", 'plain')
-            body_html = MIMEText(html, 'html')
+                # Record the MIME types of both parts - text/plain and text/html.
+                body_plain = MIMEText("Check Out Today's Free Games!F", 'plain')
+                body_html = MIMEText(html, 'html')
 
-            # Attach parts into message container.
-            msg.attach(body_plain)
-            msg.attach(body_html)
+                # Attach parts into message container.
+                msg.attach(body_plain)
+                msg.attach(body_html)
 
-            # Send the message via local SMTP server.
-            s = smtplib.SMTP('smtp.gmail.com', 587)
-            # s.set_debuglevel(1)
-            s.ehlo()
-            s.starttls()
-            s.login(fromaddr, PASSWORD)
+                # Send the message via local SMTP server.
+                s = smtplib.SMTP('smtp.gmail.com', 587)
+                # s.set_debuglevel(1)
+                s.ehlo()
+                s.starttls()
+                s.login(fromaddr, PASSWORD)
 
-            for id in range(0, len(all_emails), 400):
-                def sendmail(bcc):
-                    s.sendmail(fromaddr, toaddr + cc + bcc, str(msg))
-                sendmail(list(all_emails)[id: id + 400])
+                for id in range(0, len(all_emails), 400):
+                    def sendmail(bcc):
+                        s.sendmail(fromaddr, toaddr + cc + bcc, str(msg))
+                    sendmail(list(all_emails)[id: id + 400])
 
-            s.quit()
-    file.close()
+                s.quit()
+        file.close()
        
+main()
